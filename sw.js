61153af2b8c12a0ff082bd6100bcd4c1791abe6f
ZTrev10 <duckfatpopcorn@gmail.com>
2026-06-07 13:07:10 -0700

    b

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pt-done-v4";
+const CACHE = "pt-done-v5";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
